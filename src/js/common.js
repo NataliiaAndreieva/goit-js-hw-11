5ad09dd3cd00e5a90d7e5dd18b0ff4231125f6f5
NataliiaAndreieva <taliandreeva@gmail.com>
2023-05-07 17:24:12 +0300

    add markup

--- src/js/common.js
+++ src/js/common.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import { createMarkupCard } from './markup';
+
 const API_KEY = '36047185-510fc5005b993828af62621e5';
 const API_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}& image_type=photo & orientation=horizontal & safesearch=true & per_page=40 & page=${ page }`;
 
@@ -12,6 +14,14 @@ const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
 async function serviceImg(query, page = 1) {
-  const response = await axios.get(API_URL);
+  try {
+     const response = await axios.get(API_URL);
   return response;
+  }
+  catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('Error');
+
+  }
+  
 }
---
+++ src/js/markup.js
@@ -0,0 +1,42 @@
+import { gallery } from './common';
+
+function createMarkupCard(images) {
+    return images.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+    }) => {
+        return
+        `
+    <div class="photo-card">
+    <a href="${largeImageURL}">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes:<span>${likes}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Views:<span>${views}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Comments:<span>${comments}</span></b>
+        </p>
+        <p class="info-item">
+          <b>Downloads:<span>${downloads}</span></b>
+        </p>
+      </div>
+    </div >
+     `;
+    }
+    )
+        .join('');
+    gallery.insertAdjacentHTML('beforeend', createMarkupCard(images));
+
+}
+
+export { createMarkupCard };
